d4a32da219420c71f398aff64ce2745fd1fdee0d
Edwin <91445700+edwinbun@users.noreply.github.com>
2022-07-18 22:07:39 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pemrograman Mobile',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+// text fields' controllers
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _nimController = TextEditingController();
+
+  final CollectionReference _mahasiswa =
+  FirebaseFirestore.instance.collection('mahasiswa');
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _namaController,
+                  decoration: const InputDecoration(labelText: 'nama'),
+                ),
+                TextField(
+                  keyboardType:
+                  const TextInputType.numberWithOptions(decimal: false),
+                  controller: _nimController,
+                  decoration: const InputDecoration(
+                    labelText: 'nim',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  onPressed: () async {
+                    final String nama = _namaController.text;
+                    final double? nim =
+                    double.tryParse(_nimController.text);
+                    if (nim != null) {
+                      await _mahasiswa.add({"nama": nama, "nim": nim});
+
+                      _namaController.text = '';
+                      _nimController.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+
+        });
+  }
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+
+      _namaController.text = documentSnapshot['nama'];
+      _nimController.text = documentSnapshot['nim'].toString();
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _namaController,
+                  decoration: const InputDecoration(labelText: 'nama'),
+                ),
+                TextField(
+                  keyboardType:
+                  const TextInputType.numberWithOptions(decimal: false),
+                  controller: _nimController,
+                  decoration: const InputDecoration(
+                    labelText: 'nim',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text( 'Update'),
+                  onPressed: () async {
+                    final String nama = _namaController.text;
+                    final double? nim =
+                    double.tryParse(_nimController.text);
+                    if (nim != null) {
+
+                      await _mahasiswa
+                          .doc(documentSnapshot!.id)
+                          .update({"nama": nama, "nim": nim});
+                      _namaController.text = '';
+                      _nimController.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _delete(String productId) async {
+    await _mahasiswa.doc(productId).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('You have successfully deleted a product')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Pemrograman Mobile')),
+        ),
+        body: StreamBuilder(
+          stream: _mahasiswa.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                  streamSnapshot.data!.docs[index];
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(documentSnapshot['nama']),
+                      subtitle: Text(documentSnapshot['nim'].toString()),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () =>
+                                    _update(documentSnapshot)),
+                            IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () =>
+                                    _delete(documentSnapshot.id)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+// Add new product
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _create(),
+          child: const Icon(Icons.add),
+
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
+    );
+  }
+}
